d9a44d7c014e8b9eee3e8fc3bd7f7c11a4199d91
Mahedi0x <mahedih.cse@gmail.com>
2025-11-11 11:09:11 +0600

    all setup completed in client site

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+     
+      <h1 className='text-7xl font-bold text-center my-5'>Clean CommunityBD</h1>
+      
+    </>
+  )
+}
+
+export default App
